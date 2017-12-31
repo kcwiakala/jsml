2badfa604dfb5af6e928ce0ef512a28bcdfe0fdf
kcwiakala <konrad.cwiakala@gmail.com>
2017-12-31 17:42:45 +0100

    Added implementation of miniBatch training

--- test/mlp.js
+++ test/mlp.js
@@ -11,10 +11,11 @@ const training = require('./etc/training');
 describe('MultiLayerPerceptron', () => {
   let mlp = new MultiLayerPerceptron([2,3,1], activation.sigmoid);
   let optimizer = new Momentum(0.5, 0.75);
+  //optimizer = new Adam(0.2);
   
   it('Should be able to learn xor', () => {
     mlp.init(initializer.normal(-0.3, 0.1));
-    expect(optimizer.batchTrain(mlp, training.xor, 10000, 10, 0.01)).to.be.true;
+    expect(optimizer.batchTrain(mlp, training.xor, 10000, 0.01)).to.be.true;
     expect(mlp.output([0,0])[0]).to.be.lt(0.3);
     expect(mlp.output([1,1])[0]).to.be.lt(0.3);
     expect(mlp.output([0,1])[0]).to.be.gt(0.7);
@@ -23,18 +24,18 @@ describe('MultiLayerPerceptron', () => {
 
   it('Should be able to learn and', () => {
     mlp.init(initializer.uniform(0.5,1));
-    expect(optimizer.train(mlp, training.and, 10000, 0.001)).to.be.true;
+    expect(optimizer.batchTrain(mlp, training.and, 10000, 0.001)).to.be.true;
   });
 
   it('Should be able to learn or', () => {
     mlp.init(initializer.uniform(0.5,1));
-    expect(optimizer.train(mlp, training.or, 10000, 0.001)).to.be.true;
+    expect(optimizer.batchTrain(mlp, training.or, 10000, 0.001)).to.be.true;
   });
 
   it('Should be able to learn sin', () => {
     let mlp = new MultiLayerPerceptron([1,2,1], activation.sigmoid);
     mlp.init(initializer.uniform(0.5,1));
-    expect(optimizer.train(mlp, training.sin, 10000, 0.001)).to.be.true;
+    expect(optimizer.batchTrain(mlp, training.sin, 10000, 0.001, 5)).to.be.true;
     expect(mlp.output([0.5])[0]).to.be.closeTo(Math.sin(0.5), 0.05);
     expect(mlp.output([0.2])[0]).to.be.closeTo(Math.sin(0.2), 0.05);
     expect(mlp.output([0.3])[0]).to.be.closeTo(Math.sin(0.3), 0.05);
